095bb46d3a879b0ae7b233109bdfd72698d9344d
fanlulin <fxjzzyo@163.com>
2026-08-24 22:28:30 +0800

    关于页面；

--- app/src/main/java/com/zilin/weathercompose/ui/about/About.kt
+++ app/src/main/java/com/zilin/weathercompose/ui/about/About.kt
@@ -1,10 +1,86 @@
 package com.zilin.weathercompose.ui.about
 
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.zilin.weathercompose.LocalDrawerState
+import com.zilin.weathercompose.data.fake.FakeData
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AboutNavHostPage(modifier: Modifier = Modifier) {
+    val drawerState = LocalDrawerState.current
+    val scope = rememberCoroutineScope()
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        containerColor = Color.Transparent,
+        topBar = {
+            TopAppBar(
+                title = { Text("关于", color = Color.White) },
+                navigationIcon = {
+                    IconButton(onClick = { scope.launch { drawerState.open() } }) {
+                        Icon(Icons.Default.Menu, contentDescription = "菜单", tint = Color.White)
+                    }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color.Transparent
+                )
+            )
+        }
+    ) { innerPadding ->
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            contentAlignment = Alignment.Center
+
+        ) {
+            About()
+        }
+    }
+}
+
+
+
 
 @Composable
 fun About(modifier: Modifier = Modifier) {
-    Text("关于")
+    Column(
+        modifier = modifier.fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .padding(16.dp)
+    ) {
+        Text(FakeData.aboutStr,
+            color = MaterialTheme.colorScheme.onPrimary)
+    }
+
+}
+
+@Preview
+@Composable
+private fun AboutPreview() {
+    About()
 }
